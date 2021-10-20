ebcb43b50fe0871210502affb76aaf4774bd4f32
poloma <873800030@qq.com>
2021-10-20 23:09:08 +0800

    feat: 监听控件添加

--- infinite-scroll/src/main.ts
+++ infinite-scroll/src/main.ts
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './registerServiceWorker';
+import { Performance } from './lib/performance'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  mixins: [Performance.record(router)]
 }).$mount('#app')
