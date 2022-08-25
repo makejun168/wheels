ba007b866a5880cef38f5b60863b809393d03d1b
poloma <873800030@qq.com>
2022-08-25 18:05:25 +0800

    feat: 设置 mobx

---
+++ ui-component/my-ant-design/.storybook/config.ts
@@ -0,0 +1,10 @@
+import { configure, addDecorator, addParameters } from '@storybook/react';
+
+const loaderFn = () => {
+  const allExports = [require('../src/welcome.stories.tsx')];
+  const req = require.context('../src/components', true, /\.stories\.tsx$/);
+  req.keys().forEach(fname => allExports.push(req(fname)));
+  return allExports;
+};
+
+configure(loaderFn, module);
--- ui-component/my-ant-design/src/components/Button/button.test.tsx
+++ ui-component/my-ant-design/src/components/Button/button.test.tsx
@@ -7,8 +7,8 @@ const defaultProps = {
 }
 
 const testProps: ButtonProps = {
-  btnType: ButtonType.Primary,
-  size: ButtonSize.Large,
+  btnType: 'primary',
+  size: 'lg',
   className: 'klass'
 }
 
@@ -37,7 +37,7 @@ describe('test Button component', () => {
   })
 
   it('should render a link when btnType equals link and href is provided', () => {
-    const wrapper = render(<Button btnType={ButtonType.Link} href={"https://baidu.com"} >Link</Button>);
+    const wrapper = render(<Button btnType={'link'} href={"https://baidu.com"} >Link</Button>);
     const element = wrapper.getByText('Link');
     expect(element).toBeInTheDocument();
     expect(element.tagName).toEqual('A');
