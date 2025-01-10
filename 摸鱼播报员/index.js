cff9f2f7c23ccc820a4693a12f45b4989cfe4dc6
TENVINE\poloma <poloma@tenvine.cn>
2025-01-10 11:06:42 +0800

    feat: 提交摸鱼播报员的数据

--- 摸鱼播报员/index.js
+++ 摸鱼播报员/index.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const http = require('http');
 
 /**
@@ -10,13 +11,6 @@ class CatchFish {
         this.now = new Date(); // 当前的日期
         this.solgan = '工作学习再累，一定不要忘记摸鱼哦！\n';
         this.tips = '有事没事起身去茶水间，去厕所，去廊道走走，别老在工位上坐着，钱是老板的，但命是自己\n';
-        this.nationalHoliday = new Date('2022-10-01');
-        this.newYearHoliday = new Date('2022-01-01');
-        this.springFestival = new Date('2022-01-31');
-        this.qingMingFestival = new Date('2022-04-05');
-        this.workingHoliday = new Date('2022-05-01');
-        this.dragonBoatFestival = new Date('2022-06-03');
-        this.middleAutumnFestival = new Date('2022-09-10');
         this.oneDay = 24 * 60 * 60 * 1000; // 一天的时间
         this.resultData = ''; // 最后返回的结果
     }
@@ -72,45 +66,20 @@ class CatchFish {
             if (res.statusCode === 200) {
                 console.log(`Got response: ${res.statusCode}`);
 
-                // 因为2022 数据没有更新
                 res.on('data', (chunk) => {
+                    
                     const { result } = JSON.parse(chunk);
-
+                    
                     if (result !== null) {
                         const { data } = JSON.parse(chunk).result;
-                        const { holiday_list } = data; // 白嫖数据
-                        // 获取假期列表 2022
-                        let holidayList = [
-                            {
-                                name: '元旦节',
-                                startDay: this.newYearHoliday
-                            },
-                            {
-                                name: '春节',
-                                startDay: this.springFestival
-                            },
-                            {
-                                name: '清明节',
-                                startDay: this.qingMingFestival
-                            },
-                            {
-                                name: '劳动节',
-                                startDay: this.workingHoliday
-                            },
-                            {
-                                name: '端午节',
-                                startDay: this.dragonBoatFestival
-                            },
-                            {
-                                name: '中秋节',
-                                startDay: this.middleAutumnFestival
-                            },
-                        ];
+                        const { holidaylist, holiday_list } = data; // 白嫖数据
+                        const jsonHolidayList = JSON.parse(holidaylist)
 
                         let str = '';
 
                         // 情况一
                         // for (let holiday of holiday_list) {
+                            
                         //     if (!this.isGoneAway(this.now, new Date(holiday.startday))) {
                         //         // 判断当前时间是否为 假期以后，如果是则 跳过
                         //         str += this.getGapMsg(holiday.name, this.getDateGap(this.now, new Date(holiday.startday)))
@@ -118,14 +87,15 @@ class CatchFish {
                         // }
 
                         // 情况二
-                        for (let holiday of holidayList) {
-                            if (!this.isGoneAway(this.now, holiday.startDay)) {
+                        for (let holiday of jsonHolidayList) {
+                            if (!this.isGoneAway(this.now, new Date(holiday.startday))) {
                                 // 判断当前时间是否为 假期以后，如果是则 跳过
-                                str += this.getGapMsg(holiday.name, this.getDateGap(this.now, holiday.startDay))
+                                str += this.getGapMsg(holiday.name, this.getDateGap(this.now, new Date(holiday.startday)))
                             }
                         }
 
                         this.resultData = tips + str;
+                        // console.log(str)
                         console.log(this.resultData); // 最后结果 打印到控制台
                     } else {
                         console.log('没有假期数据')
@@ -134,7 +104,6 @@ class CatchFish {
             } else {
                 console.log('请求发生错误')
             }
-            // console.log(res);
         })
     }
 
@@ -146,7 +115,7 @@ class CatchFish {
         // 判断当前是在哪个位置，判断当前的时间如果超过则显示 0 天
         console.log(result);
 
-        this.requestData(2021, result);
+        this.requestData(2025, result);
     }
 }
 
